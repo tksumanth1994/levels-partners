82e8a0567709124100106a61b3d6252afa70bcad
Krishna Sumanth <tksumanth1994@gmail.com>
2021-05-04 23:10:01 +0530

    fix: Update date

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -61,7 +61,7 @@ export default function main({ totalUsers }: Props): ReactElement {
                 d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
               />
             </svg>
-            8th April, 2021
+            4th May, 2021
           </span>
         </div>
         <nav
